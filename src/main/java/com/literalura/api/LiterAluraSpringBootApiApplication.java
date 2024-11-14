cb5544297f4ff047c7b46cab2df695ec4d21eeed
Alejo-Santis <alejosantismk11@gmail.com>
2024-11-13 19:38:46 -0500

    First commit for challenge-literalura

---
+++ src/main/java/com/literalura/api/LiterAluraSpringBootApiApplication.java
@@ -0,0 +1,13 @@
+package com.literalura.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiterAluraSpringBootApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LiterAluraSpringBootApiApplication.class, args);
+	}
+
+}
